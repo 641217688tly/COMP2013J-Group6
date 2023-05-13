1be42ed07fe546dbae5132b380e6d0ce6afbb0bc
LiYanTao <641217688tly@gmail.com>
2023-05-13 12:42:29 +0800

    Solve the bug of submitting single Course message (Note! Submitting Excel File still exists BUG!)

--- src/main/java/ie/ucd/comp2013J/web/InsertServlet.java
+++ src/main/java/ie/ucd/comp2013J/web/InsertServlet.java
@@ -8,6 +8,7 @@ import ie.ucd.comp2013J.service.ClassroomService;
 import ie.ucd.comp2013J.service.CourseService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +20,7 @@ import java.util.ArrayList;
 
 
 @WebServlet(urlPatterns = "/insertServlet")
+@MultipartConfig
 public class InsertServlet extends HttpServlet {
     private final ClassroomService classroomservice = new ClassroomService();
     private final CourseService courseService = new CourseService();
@@ -32,26 +34,29 @@ public class InsertServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
-        Part filePart = request.getPart("file"); //判断接收的是Excel文件还是单个Course的信息?
-        if (filePart != null && filePart.getSize() > 0) {
-            String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // 获取文件名，包括扩展名
-            String fileExtension = fileName.substring(fileName.lastIndexOf(".")).toLowerCase(); // 获取文件扩展名
-            if (fileExtension.equals(".xls") || fileExtension.equals(".xlsx")) { // 用户上传的是一个Excel文件，处理Excel文件...
-                ArrayList<Course> courses = courseService.insertExcelFile(filePart.getInputStream());//先插入课程
-                ArrayList<Classroom> classrooms = classroomservice.insertExcelFile(filePart.getInputStream());//再插入教室
-                boolean flag1 = classroomCourseService.insertExcelFile(courses, classrooms); //再根据教室和课程进行连接
-                if (flag1) {
-                    request.setAttribute("upload_message2", "Excel内的所有课程信息都已被正确上传!");
-                    request.getRequestDispatcher("/insert.jsp").forward(request, response);
+        String contentType = request.getContentType();
+        if (contentType != null && contentType.startsWith("multipart/form-data")) {
+            Part filePart = request.getPart("file");
+            if (filePart != null && filePart.getSize() > 0) {
+                String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+                String fileExtension = fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
+                if (fileExtension.equals(".xls") || fileExtension.equals(".xlsx")) {
+                    ArrayList<Course> courses = courseService.insertExcelFile(filePart.getInputStream());
+                    ArrayList<Classroom> classrooms = classroomservice.insertExcelFile(filePart.getInputStream());
+                    boolean flag1 = classroomCourseService.insertExcelFile(courses, classrooms);
+                    if (flag1) {
+                        request.setAttribute("upload_message2", "Excel内的所有课程信息都已被正确上传!");
+                        request.getRequestDispatcher("/insert.jsp").forward(request, response);
+                    } else {
+                        request.setAttribute("upload_message2", "抱歉,Excel课表文件解析存在错误,部分课程可能没有被正确上传");
+                        request.getRequestDispatcher("/insert.jsp").forward(request, response);
+                    }
                 } else {
-                    request.setAttribute("upload_message2", "抱歉,Excel课表文件解析存在错误,部分课程可能没有被正确上传");
+                    request.setAttribute("upload_message2", "The uploaded file format is incorrect, please upload Excel file!");
                     request.getRequestDispatcher("/insert.jsp").forward(request, response);
                 }
-            } else { // 用户上传的不是一个Excel文件，返回错误信息...
-                request.setAttribute("upload_message2", "The uploaded file format is incorrect, please upload Excel file!");
-                request.getRequestDispatcher("/insert.jsp").forward(request, response);
             }
-        } else { // 用户上传的是单个课程的信息，处理课程信息...
+        } else {
             Course course = new Course();
             Classroom classroom = new Classroom();
             String name = request.getParameter("name");
@@ -59,7 +64,7 @@ public class InsertServlet extends HttpServlet {
             String endWeek = request.getParameter("endWeek");
             String weekDay = request.getParameter("weekDay");
             String schooltime = request.getParameter("schooltime");
-            String detail = request.getParameter("detail"); //可以为空
+            String detail = request.getParameter("detail");
             String classroomNumber = request.getParameter("classroomNumber");
 
             if (name != null && startWeek != null && endWeek != null && weekDay != null && schooltime != null && classroomNumber != null) {
@@ -72,14 +77,14 @@ public class InsertServlet extends HttpServlet {
                 classroom.setNumber(Integer.parseInt(classroomNumber));
 
                 boolean flag2 = classroomCourseService.insertClassroomCourse1(courseService.insertCourse(course), classroomservice.insertClassroom(classroom));
-                if (flag2) { //插入成功,数据库中之前不存在该课程信息
+                if (flag2) {
                     request.setAttribute("upload_message1", "Upload Successfully!");
                     request.getRequestDispatcher("/insert.jsp").forward(request, response);
-                } else { //插入失败,数据库中已经存在该课程信息
+                } else {
                     request.setAttribute("upload_message1", "Failed To Upload!");
                     request.getRequestDispatcher("/insert.jsp").forward(request, response);
                 }
-            } else { //所需的参数缺失,上传失败
+            } else {
                 request.setAttribute("upload_message1", "Failed To Upload!");
                 request.getRequestDispatcher("/insert.jsp").forward(request, response);
             }
